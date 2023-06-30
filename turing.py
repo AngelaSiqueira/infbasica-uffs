765c83a81b50b1416dca3cd5effbd0ff6112d178
Djonatan Bonelli <100030801+ValtemirJr@users.noreply.github.com>
2023-06-30 15:55:21 -0300

    commit

--- turing.py
+++ turing.py
@@ -1,25 +1,25 @@
 def resumo():
-    mensagem = "Alan Mathison Turing  foi um matemático britânico, pioneiro da computação e considerado o pai da ciência computacional e da inteligência artificial."
+    mensagem = "Grace Brewster Murray Hopper nasceu em Nova York, EUA, em 1906. Sendo a criadora das primeiras linguagens computacionais “humanas”, que possibilitou a interpretação computacional de comando em inglês no lugar de apenas números e símbolos."
     return mensagem
 
 
 def doutorado():
-    mensagem = ""
+    mensagem = "Esta norte-americana obteve um doutorado em Matemática por Yale em 1934 e, quando os EUA entraram na Segunda Guerra Mundial, abandonou seu trabalho de professora de matemática e ingressou na Marinha, onde chegou à patente de contra-almirante."
     return mensagem
 
 
 def contribuicoes():
-    mensagem = ""
+    mensagem = "Criou a Linguagem Comum Orientada para Negócios (COBOL, na sigla em inglês), a primeira linguagem complexa de computador, que é utilizada até hoje por empresas de todo o mundo. Também ficou conhecida por ter batizado o primeiro bug de computador da história."
     return mensagem
 
 
 def artigos():
-    mensagem = ""
+    mensagem = "Artigos dela? não achei"
     return mensagem
 
 
 def citacoes():
-    mensagem = ""
+    mensagem = "Se é uma boa ideia, prossiga e leve-a adiante. É muito mais fácil pedir desculpas do que conseguir a permissão necessária.\nPara mim Programação é mais do que uma arte prática importante. É também um empreendimento gigantesco nos fundamentos do conhecimento.\nHumans são alérgicos para mudar. Eles adoram dizer: 'Nós sempre fizemos assim.' Eu tento lutar isso. É por isso que eu tenho um relógio na minha parede que corre no sentido anti-horário."
     return mensagem
 
 
